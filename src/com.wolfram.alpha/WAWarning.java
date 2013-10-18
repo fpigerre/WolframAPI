e984f215f43760d34976d074ca9d4b16ab093cd2
psgs <fpigerre@gmail.com>
2013-10-19 09:08:17 +1000

    Initial Commit

---
+++ src/com.wolfram.alpha/WAWarning.java
@@ -0,0 +1,15 @@
+/*
+ * Created on Dec 8, 2009
+ *
+ */
+package com.wolfram.alpha;
+
+
+public interface WAWarning {
+
+    String getType();
+
+    String getText();
+
+    String[][] getAttributes();
+}
